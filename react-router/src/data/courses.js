82f941c8b5b4d273c20c2672dd6e2774653c383e
MihailSuvorov555 <128319056+MihailSuvorov555@users.noreply.github.com>
2023-03-24 01:29:55 +0500

    Create courses.js

---
+++ react-router/src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+    {
+        title: 'Complete react Guide',
+        slug: 'react',
+        id:523
+    },
+    {
+        title: 'Complete JavaScript Guide',
+        slug: 'JavaScript',
+        id:323
+    },
+    {
+        title: 'Complete Python Guide',
+        slug: 'Python',
+        id:325
+    },
+    {
+        title: 'Complete Node.js Guide',
+        slug: 'node-js',
+        id:232
+    }
+]
+
+export default courses
